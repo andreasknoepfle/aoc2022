bd94bb78492eab37d0be2eeb29ec825e5155f62f
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2022-12-19 23:29:14 +0100

    wip

--- lib/aoc2022/door12.ex
+++ lib/aoc2022/door12.ex
@@ -6,94 +6,40 @@ defmodule Aoc2022.Door12 do
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.to_charlist/1)
     |> Stream.with_index()
-    |> Enum.reduce({%{}, nil}, fn {row, index}, {heightmap, goal} ->
+    |> Enum.reduce({%{}, nil, nil}, fn {row, index}, {heightmap, start, goal} ->
       row
       |> Enum.with_index()
-      |> Enum.reduce({heightmap, goal}, &convert_row(index, &1, &2))
+      |> Enum.reduce({heightmap, start, goal}, &convert_row(index, &1, &2))
     end)
     |> find_shortest_path()
   end
 
-  @start ?a - 1
-  @goal ?z + 1
-  defp convert_row(y, {?S, x}, {map, goal}),
-    do: {Map.put(map, {x, y}, {@start, 0, nil}), goal}
+  defp convert_row(y, {?S, x}, {map, _, goal}),
+    do: {Map.put(map, {y, x}, 0), {y, x}, goal}
 
-  defp convert_row(y, {?E, x}, {map, _}),
-    do: {Map.put(map, {x, y}, {@goal, :infinity, nil}), {x, y}}
+  defp convert_row(y, {?E, x}, {map, start, _}),
+    do: {Map.put(map, {y, x}, 25), start, {y, x}}
 
-  defp convert_row(y, {height, x}, {map, goal}),
-    do: {Map.put(map, {x, y}, {height, :infinity, nil}), goal}
+  defp convert_row(y, {height, x}, {map, start, goal}),
+    do: {Map.put(map, {y, x}, height - ?a), start, goal}
 
-  defp find_shortest_path({heightmap, goal}) do
-    find_shortest_path(heightmap, MapSet.new(Map.keys(heightmap)), goal)
-  end
+  defp find_shortest_path({heightmap, start, goal}) do
+    graph = :digraph.new()
 
-  defp find_shortest_path(heightmap, remaining, goal) do
-    {{x, y} = min, {level, distance, _}} =
+    Enum.each(heightmap, fn {v, _} ->
+      :digraph.add_vertex(graph, v)
+    end)
+
+    Enum.each(heightmap, fn {{y, x} = v, level} ->
       heightmap
-      |> Map.take(remaining |> MapSet.to_list())
-      |> Enum.min_by(fn {_, {_, value, _}} -> value end)
-
-    if min == goal do
-      heightmap
-      |> print_debug(goal)
-      |> Map.get(goal)
-      |> elem(1)
-    else
-      remaining = MapSet.delete(remaining, min)
-
-      heightmap
-      |> Map.take([{x - 1, y}, {x, y - 1}, {x + 1, y}, {x, y + 1}])
-      |> Enum.filter(fn {key, {other, _, _}} ->
-        level + 1 >= other and MapSet.member?(remaining, key)
+      |> Map.take([{y + 1, x}, {y - 1, x}, {y, x - 1}, {y, x + 1}])
+      |> Enum.filter(fn {_, height} -> level + 1 >= height end)
+      |> Enum.each(fn {new, _} ->
+        :digraph.add_edge(graph, v, new)
       end)
-      |> Enum.reduce(heightmap, fn {key, {height, other_distance, _}}, map ->
-        new_distance = distance + 1
+    end)
 
-        if new_distance < other_distance do
-          Map.put(map, key, {height, new_distance, min})
-        else
-          map
-        end
-      end)
-      |> find_shortest_path(remaining, goal)
-    end
-  end
-
-  defp print_debug(heightmap, goal) do
-    max_x = heightmap |> Map.keys() |> Enum.map(&elem(&1, 0)) |> Enum.max()
-    max_y = heightmap |> Map.keys() |> Enum.map(&elem(&1, 1)) |> Enum.max()
-    path = build_path(heightmap, [goal])
-    sparse = Map.take(heightmap, path)
-
-    for y <- 0..max_y do
-      for x <- 0..max_x do
-        Map.get(sparse, {x, y})
-        |> case do
-          nil -> "."
-          {_, _, nil} -> "S"
-          {_, _, {a, b}} when a + 1 == x and b == y -> ">"
-          {_, _, {a, b}} when a - 1 == x and b == y -> "<"
-          {_, _, {a, b}} when a == x and b - 1 == y -> "^"
-          {_, _, {a, b}} when a == x and b + 1 == y -> "v"
-        end
-        |> IO.write()
-      end
-
-      IO.write("\n")
-    end
-
-    heightmap
-  end
-
-  def build_path(_, [nil | route]) do
-    route
-  end
-
-  def build_path(heightmap, [last | _] = all) do
-    {_, _, next} = Map.get(heightmap, last)
-    build_path(heightmap, [next | all])
+    length(:digraph.get_short_path(graph, start, goal)) - 1
   end
 
   def run_b(_stream) do
